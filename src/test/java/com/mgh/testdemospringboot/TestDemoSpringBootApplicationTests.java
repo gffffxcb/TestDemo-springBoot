e8f8bceecf7439c348f0ebf12c6fe2e311893b7b
gffffxcb <34471180+gffffxcb@users.noreply.github.com>
2022-02-04 18:51:03 +0800

    Update TestDemoSpringBootApplicationTests.java

--- src/test/java/com/mgh/testdemospringboot/TestDemoSpringBootApplicationTests.java
+++ src/test/java/com/mgh/testdemospringboot/TestDemoSpringBootApplicationTests.java
@@ -3,6 +3,7 @@ package com.mgh.testdemospringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 //test 修改后提交！！
+//在线修改后pull到本地
 @SpringBootTest
 class TestDemoSpringBootApplicationTests {
 
